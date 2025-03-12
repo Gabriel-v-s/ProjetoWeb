55027b247dd741d24934a8df8742f277577d22b8
Gabriel Vitor <gabriel.v.santos90@gmail.com>
2025-03-12 15:28:48 -0300

    delimitando components e pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Cadastro from "../components/Cadastro/cadastroPage";
 import Login from "../components/Login/loginPage";
+import Home from "../pages/Home";
 
 function MainRoutes() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
